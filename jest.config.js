8d9f2dc41850514574969f947bb09e5ee8d927d7
Gabriel Loiácono <gabriel.vloiacono@gmail.com>
2020-01-17 00:50:02 -0300

    test: added testURL config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   preset: 'ts-jest',
   watchPathIgnorePatterns: ['/node_modules/'],
-  rootDir: __dirname
+  rootDir: __dirname,
+  testURL: 'https://unquery.com/current-url'
 }
