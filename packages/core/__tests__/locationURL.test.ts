eec1d559e5bfe5e2351b177db8041fd0c7d01388
Gabriel Loiácono <gabriel.vloiacono@gmail.com>
2020-01-17 00:08:52 -0300

    test: added tests to replaceLocation and addLocation

--- packages/core/__tests__/locationURL.test.ts
+++ packages/core/__tests__/locationURL.test.ts
@@ -1,6 +1,13 @@
 import Unquery, { clearLocationURL, addLocationURL } from '..'
+import { replaceLocationURL } from '../src/locationURL'
+
+const url = 'https://unquery.com/current-url'
 
 describe('addLocationURL', () => {
+  afterEach(() => {
+    clearLocationURL()
+  })
+
   const query = Unquery('?foo=bar', {
     foo: Unquery.string()
   })
@@ -20,12 +27,34 @@ describe('addLocationURL', () => {
     expect(location.href).toContain('?foo=bar#value')
   })
 
-  it('should update without remove hash', () => {
+  it('should clear and update while keep hash', () => {
+    location.hash = 'foo'
+
+    expect(location.href).toBe(`${url}${location.hash}`)
+
+    addLocationURL('?foo=baz')
+
+    expect(location.href).toBe(`${url}?foo=baz${location.hash}`)
+
+    addLocationURL('bar=1')
     location.hash = ''
-    expect(location.href.endsWith('?foo=bar')).toBe(true)
 
-    clearLocationURL()
+    expect(location.href).toBe(`${url}?foo=baz&bar=1${location.hash}`)
 
+    replaceLocationURL('baz=42')
+
+    expect(location.href).toBe(`${url}?baz=42${location.hash}`)
+  })
+
+  it('should update without remove hash', () => {
+    location.hash = 'baz'
+
+    expect(location.href.endsWith('#baz')).toBe(true)
     expect(location.search).toBe('')
+
+    addLocationURL('bar=123')
+
+    expect(location.search).toBe('?bar=123')
+    expect(location.href.endsWith('?bar=123#baz')).toBe(true)
   })
 })
