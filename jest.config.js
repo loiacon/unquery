1cb2dbdbf62bae9527ea6e631d66c77a1795ab1c
Gabriel Loiácono <gabriel.vloiacono@gmail.com>
2020-01-21 23:29:34 -0300

    test: added global threshold config

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,13 @@ module.exports = {
   preset: 'ts-jest',
   watchPathIgnorePatterns: ['/node_modules/'],
   rootDir: __dirname,
-  testURL: 'https://unquery.com/current-url'
+  testURL: 'https://unquery.com/current-url',
+  coverageThreshold: {
+    global: {
+      statements: 100,
+      branches: 100,
+      functions: 100,
+      lines: 100
+    }
+  }
 }
