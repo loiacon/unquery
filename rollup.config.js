764086571baef07e03144c1f01a2489b33372fe9
Gabriel Loiácono <gabriel.vloiacono@gmail.com>
2020-01-25 02:56:53 -0300

    feat: created umd and iife formats

--- rollup.config.js
+++ rollup.config.js
@@ -5,13 +5,23 @@ export default {
   input: 'packages/core/index.ts',
   output: [
     {
-      file: 'dist/index.js',
+      file: 'dist/index.cjs.js',
       format: 'cjs',
       exports: 'named'
     },
     {
       file: 'dist/index.es.js',
       format: 'es'
+    },
+    {
+      file: 'dist/index.umd.js',
+      format: 'umd',
+      name: 'Unquery'
+    },
+    {
+      file: 'dist/index.js',
+      format: 'iife',
+      name: 'Unquery'
     }
   ],
   plugins: [terser(), typescript({ clean: true })]
