82830c41030565d05a80de676881c9de2c47f3e0
Gabriel Loiácono <gabriel.vloiacono@gmail.com>
2020-01-25 03:57:33 -0300

    fix: fixed build

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,26 @@
 import typescript from 'rollup-plugin-typescript2'
 import { terser } from 'rollup-plugin-terser'
+import pkg from './package.json'
 
 export default {
   input: 'packages/core/index.ts',
   output: [
     {
-      file: 'dist/index.cjs.js',
-      format: 'cjs',
-      exports: 'named'
-    },
-    {
-      file: 'dist/index.es.js',
-      format: 'es'
-    },
-    {
-      file: 'dist/index.umd.js',
+      file: `dist/${pkg.name}.umd.js`,
       format: 'umd',
       name: 'Unquery'
     },
     {
-      file: 'dist/index.js',
+      file: `dist/${pkg.name}.esm.js`,
+      format: 'es'
+    },
+    {
+      file: `dist/${pkg.name}.cjs.js`,
+      format: 'cjs',
+      exports: 'named'
+    },
+    {
+      file: `dist/${pkg.name}.iife.js`,
       format: 'iife',
       name: 'Unquery'
     }
