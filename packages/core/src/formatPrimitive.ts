3434d22e63aa7a5234cb01f13d4125ebaf418cee
Gabriel Loiácono <gabriel.vloiacono@gmail.com>
2020-03-08 20:38:26 -0300

    feat(core): deprecated Unquery.date

--- packages/core/src/formatPrimitive.ts
+++ packages/core/src/formatPrimitive.ts
@@ -1,39 +1,7 @@
 import { UnqueryType, UnqueryTypeReturn } from './types'
-import { dateTokens } from './utils/date'
-import { provideOption } from './unqueryOptions'
-import warn from './utils/warn'
 import { safeDecodeURI } from './utils'
 
-const parseOptional = (val: string) => `0${val || 0}`.slice(-2)
-
-type DateParser = {
-  day: string
-  month: string
-  year: string
-  hours?: string
-  minutes?: string
-  seconds?: string
-}
-
-const parseDate = (val: string, pattern: string) => {
-  const date: DateParser = { day: '', month: '', year: '' }
-  for (let i = 0; i < pattern.length; i++) {
-    const token = dateTokens[pattern[i]]
-    if (token) {
-      date[token[1]] += val[i]
-    }
-  }
-
-  date.hours = parseOptional(date.hours)
-  date.minutes = parseOptional(date.minutes)
-  date.seconds = parseOptional(date.seconds)
-
-  return new Date(
-    `${date.year}-${date.month}-${date.day}T${date.hours}:${date.minutes}:${date.seconds}.000Z`
-  )
-}
-
-function formatPrimitive(value: string, { type, pattern }: UnqueryTypeReturn) {
+function formatPrimitive(value: string, { type }: UnqueryTypeReturn) {
   if (value == null) {
     return null
   }
@@ -51,16 +19,6 @@ function formatPrimitive(value: string, { type, pattern }: UnqueryTypeReturn) {
     return Boolean(value)
   }
 
-  if (type & UnqueryType.DATE) {
-    pattern = pattern || provideOption('parsePattern', 'pattern')
-
-    const date = parseDate(value, pattern)
-    if (isNaN(date.getTime())) {
-      warn(`Provided value "${value}" is not matching pattern "${pattern}".`)
-      return value
-    }
-    return date
-  }
   return value
 }
 
